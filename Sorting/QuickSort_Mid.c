a69ca4d5fae08f9709a6a7ab35e31ca8836d10d2
jahnvinair <141730370+jahnvinair@users.noreply.github.com>
2023-09-01 09:29:02 +0530

    Create QuickSort_Mid.c

---
+++ Sorting/QuickSort_Mid.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int partition(int arr[], int low, int high) {
+    int mid = (low + high) / 2;
+    int pivot = arr[mid];
+    int i = low - 1;
+
+    for (int j = low; j <= high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    int temp = arr[i+1];
+    arr[i+1] = arr[mid];
+    arr[mid] = temp;
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    quickSort(arr, 0, n - 1);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
