63d406cff3c3ad30131105c11b922bdf8ca1026d
jahnvinair <141730370+jahnvinair@users.noreply.github.com>
2023-09-01 08:56:01 +0530

    Update Iterative_Merge.c

--- Iterative_Merge.c
+++ Iterative_Merge.c
@@ -1,21 +1,52 @@
 #include <stdio.h>
 
-void merge(int arr[], int l, int m, int r) {
-    int n1 = m - l + 1, n2 = r - m, i, j, k;
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
     int L[n1], R[n2];
-    for (i = 0; i < n1; i++) L[i] = arr[l + i];
-    for (j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
-    i = j = 0; k = l;
-    while (i < n1 && j < n2) arr[k++] = (L[i] <= R[j]) ? L[i++] : R[j++];
-    while (i < n1) arr[k++] = L[i++];
-    while (j < n2) arr[k++] = R[j++];
+
+    for (int i = 0; i < n1; i++) {
+        L[i] = arr[left + i];
+    }
+    for (int j = 0; j < n2; j++) {
+        R[j] = arr[mid + 1 + j];
+    }
+
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
 }
 
-void mergeSort(int arr[], int n) {
+void iter_MergeSort(int arr[], int n) {
     for (int curr_size = 1; curr_size <= n - 1; curr_size = 2 * curr_size) {
         for (int left_start = 0; left_start < n - 1; left_start += 2 * curr_size) {
-            int mid = left_start + curr_size - 1, right_end = left_start + 2 * curr_size - 1;
-            if (right_end > n - 1) right_end = n - 1;
+            int mid = left_start + curr_size - 1;
+            int right_end = left_start + 2 * curr_size - 1;
+            if (right_end > n - 1) {
+                right_end = n - 1;
+            }
             merge(arr, left_start, mid, right_end);
         }
     }
@@ -24,8 +55,14 @@ void mergeSort(int arr[], int n) {
 int main() {
     int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
     int n = sizeof(arr) / sizeof(arr[0]);
-    mergeSort(arr, n);
-    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+
+    iter_MergeSort(arr, n);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
     printf("\n");
+
     return 0;
 }
